a0d018a80a2bcea07b27766940da606370c0307a
emanuelchristo <emanuelchristochris@gmail.com>
2021-09-26 06:03:30 +0530

    cors all

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const httpServer = http.createServer(app)
-const io = socketio(httpServer)
+const io = socketio(httpServer, { cors: true, origins: ['*:*'] })
 
 // Data
 const users = []
