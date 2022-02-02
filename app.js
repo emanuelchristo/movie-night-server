2a9621bc6ad033caa648f05311ebb8ec658a6fd0
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-02 11:29:52 +0530

    change default port to 5300

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser')
 // const rooms = new Datastore({ filename: './data/rooms', autoload: true })
 
 // Constants
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5300
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
 
 // Creating server
